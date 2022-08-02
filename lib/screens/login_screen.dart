65ec870da0199e9bf59846b0d588e376153a5828
mgoldp <mgoldp2013@gmail.com>
2022-08-02 16:50:55 -0400

    Log in form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,28 +9,81 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: AuthBackground(
           child: SingleChildScrollView(
-        child: Column(children: [
-          const SizedBox(
-            height: 200,
-          ),
-          CardContainer(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 200,
+            ),
+            CardContainer(
               child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Log In',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const _LoginForm(),
+                ],
               ),
-              Text(
-                'Log In',
-                style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Crear una nueva cuenta',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Text('Form'),
-            ],
-          )),
-        ]),
+            ),
+          ],
+        ),
       )),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        // TODO: Manetener la referencia al KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+                ),
+                hintText: 'john.doe@gmail.com',
+                labelText: 'Correo electrónico',
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                prefixIcon: Icon(
+                  Icons.alternate_email_sharp,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
