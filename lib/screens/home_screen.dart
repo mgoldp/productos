046c0729df158b3f4be6afff64772ec749bd1c7c
mgoldp <mgoldp2013@gmail.com>
2022-08-14 02:02:35 -0400

    HomeScreen Product Page Card Design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Productos')),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: ((context, index) => const ProductCard()),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
