cf095a79bb9082cffb53e32b9e1026db5460e7b5
mgoldp <mgoldp2013@gmail.com>
2022-08-27 01:22:25 -0400

    Product Screen Part 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productos App',
-      initialRoute: 'product',
+      initialRoute: 'homescreen',
       routes: {
         'login': (_) => const LoginScreen(),
         'homescreen': (_) => const HomeScreen(),
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/product_image.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -8,15 +9,125 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Stack(
-            children: const [
-              ProductImage(),
-            ],
-          )
-        ],
-      )),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                const ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                    onPressed: () {
+                      // TODO: Cámara o galería
+                    },
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const _ProductForm(),
+            const SizedBox(
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.save_outlined),
+        onPressed: () {
+          // TODO: Guardar producto
+        },
+      ),
     );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        height: 280,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+            child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'Nombre del Producto',
+                labelText: 'Nombre:',
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '\$150',
+                labelText: 'Precio',
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SwitchListTile.adaptive(
+              value: true,
+              title: const Text('Disponible'),
+              activeColor: Colors.indigo,
+              onChanged: (value) {
+                // TODO: pendiente
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          bottomRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: const Offset(0, 5),
+            blurRadius: 5,
+          ),
+        ],
+      );
 }
