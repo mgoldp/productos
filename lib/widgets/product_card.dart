ce9b4e39e5f302e67a6d1e8da876eaca1bd4ed67
mgoldp <mgoldp2013@gmail.com>
2022-09-06 23:19:23 -0400

    null images handling

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -168,12 +168,16 @@ class _BackgroundImage extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
-          // TODO: Fix producto cuando no hay imagen
-          placeholder: const AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage(url!),
-          fit: BoxFit.cover,
-        ),
+        child: url == null
+            ? const Image(
+                image: AssetImage('assets/no-image.png'),
+                fit: BoxFit.cover,
+              )
+            : FadeInImage(
+                placeholder: const AssetImage('assets/jar-loading.gif'),
+                image: NetworkImage(url!),
+                fit: BoxFit.cover,
+              ),
       ),
     );
   }
