1feb581c79953f0263f7674e1f72a915ca6a7576
mgoldp <mgoldp2013@gmail.com>
2022-11-06 01:02:01 -0300

    logout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:productos_app/models/models.dart';
 import 'package:productos_app/screens/loading.dart';
+import 'package:productos_app/services/auth_service.dart';
 import 'package:productos_app/services/product_service.dart';
 import 'package:productos_app/widgets/widgets.dart';
 import 'package:provider/provider.dart';
@@ -11,12 +12,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productsServices = Provider.of<ProductService>(context);
+    final authService = Provider.of<AuthService>(context, listen: false);
 
     if (productsServices.isLoading) return const LoadingScreen();
 
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Productos')),
+        actions: [
+          IconButton(
+            onPressed: () {
+              authService.logout();
+              Navigator.pushReplacementNamed(context, 'login');
+            },
+            icon: const Icon(Icons.login_outlined),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: productsServices.products.length,
