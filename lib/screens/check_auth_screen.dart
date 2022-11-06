2665eb4cd6e458e72fd2263726b7e5a84d551f73
mgoldp <mgoldp2013@gmail.com>
2022-11-06 00:55:59 -0300

    delete transition between screens

--- lib/screens/check_auth_screen.dart
+++ lib/screens/check_auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
 import 'package:productos_app/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
@@ -16,13 +17,25 @@ class CheckAuthScreen extends StatelessWidget {
           builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
             if (!snapshot.hasData) return const Text('Espere...');
 
-            //if (snapshot.data == '') {
-            //
-            //}
-
-            Future.microtask(() {
-              Navigator.pushReplacementNamed(context, 'login');
-            });
+            if (snapshot.data == '') {
+              Future.microtask(() {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: (_, __, ___) => const LoginScreen(),
+                      transitionDuration: const Duration(seconds: 0)),
+                );
+              });
+            } else {
+              Future.microtask(() {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: (_, __, ___) => const HomeScreen(),
+                      transitionDuration: const Duration(seconds: 0)),
+                );
+              });
+            }
 
             return Container();
           },
